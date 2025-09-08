098b1ec75fc9673d46f61356a781f2623d72079b
Jeff <jefferson.lopes@germinare.org.br>
2025-09-08 18:31:15 -0300

    build: Adicionando arquivos padroes de cores no projeto.

--- src/components/sidebar/index.jsx
+++ src/components/sidebar/index.jsx
@@ -5,8 +5,8 @@ export function Sidebar({ children }) {
     const [expanded, setExpanded] = useState(true);
 
     return (
-        <aside className="sidebar">
-            <nav className="h-full flex flex-col bg-white border-r shadow-sm">
+        <aside className="sidebar h-screen">
+            <nav className="h-full flex flex-col bg-primary border-r shadow-sm">
                 {/* Header */}
                 <div className="p-4 pb-2 flex justify-between items-center">
                     <img
