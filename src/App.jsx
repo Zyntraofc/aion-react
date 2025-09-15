f0ac7c0ebdb36ff7f47d3afe239ae366c1d04a4a
RaphaelaMariaGomes <raphaelamsg@gmail.com>
2025-09-15 07:22:52 -0300

    feat: Adicionando ações rápidas e recentes

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,16 @@
-import { SidebarItem } from "./components/sidebarItem/index.jsx";
-import { Sidebar } from "./components/sidebar/index.jsx";
+import { SidebarItem } from "./components/sidebarItem";
+import { Sidebar } from "./components/sidebar";
 import { LayoutDashboard, BarChart3 } from "lucide-react";
-import Header from "./components/header/index.jsx";
+import Header from "./components/header";
+import QuickInformations from "./components/quickInformations";
+import Title from "./components/title";
+import QuickActions from "./components/quickActions";
+import Recent from "./components/recent";
+
 
 export default function App() {
     return (
-        <main className="flex">
+        <main className="flex min-h-screen bg-gray-100">
             <Sidebar>
                 <SidebarItem
                     icon={<LayoutDashboard size={20} />}
@@ -17,8 +22,22 @@ export default function App() {
                 <SidebarItem icon={<BarChart3 size={20} />} text="Colaboradores" />
                 <SidebarItem icon={<BarChart3 size={20} />} text="Onboarding" />
             </Sidebar>
-            <Header />
+            <div className="flex-1 flex flex-col">
+                <Header />
+                <Title title="Visão Geral" />
+                <QuickInformations pendentes={7} aprovadas={3} colaboradoresAtivos={5} taxaAprovacao={80} analise={2}/>
+                <div className="flex flex-1 p-4 space-x-4">
+                <div className="p-4 w-3/5">
+                    <QuickActions />
 
+                </div>
+                <div className="p-4 w-2/5">
+                    <Recent />
+                </div>
+                </div>
+
+
+            </div>
         </main>
     );
 }
