c79b2d00bafd21c45eec1413a3b1c8b89b53aa8b
Jeff <jefferson.lopes@germinare.org.br>
2025-09-30 09:00:10 -0300

    style: Estilizando a SideBar e icluindo o .svg necessário para o ícone.

--- src/components/sidebarItem/index.jsx
+++ src/components/sidebarItem/index.jsx
@@ -4,7 +4,8 @@ export function SidebarItem({ icon, text, active, alert, onClick }) {
             className={
                 "relative flex items-center mt-2 py-2 px-4 h-12 my-1 font-medium rounded-[15px] cursor-pointer transition-colors " +
                 (active
-                    ? "bg-selected-item text-white"
+                    ? "bg-[radial-gradient(circle_at_top_right,_#7B84FF,_#3A47FF)] text-white"
+                    + " backdrop-blur-lg border-1 border-white/30 shadow-lg" // Adicione estas classes para o efeito Glassmorphism
                     : "hover:bg-hover-item text-white")
             }
             onClick={onClick}
@@ -12,7 +13,7 @@ export function SidebarItem({ icon, text, active, alert, onClick }) {
             {icon}
             <span className="w-52 ml-3 text-[0.9rem]">{text}</span>
             {alert && (
-                <div className="absolute right-2 w-2 h-2 bg-indigo-400"/>
+                <div className="absolute right-2 w-2 h-2 bg-indigo-400 rounded-full" />
             )}
         </li>
     );
