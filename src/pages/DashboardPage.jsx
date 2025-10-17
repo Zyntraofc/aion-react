1f3fdabf29ffeca54234cb4cd4f5821bdd938e20
jeffersonlopes-ieg <jeff@2025>
2025-10-17 14:32:17 -0300

    feat: Incluindo todas as pages do projeto.

--- src/pages/DashboardPage.jsx
+++ src/pages/DashboardPage.jsx
@@ -8,36 +8,15 @@ import QuickInformations from '../components/quickInformations/index.jsx';
 function DashboardPage() {
   return(
     <div className='flex-1 flex flex-col'>
-      <Title title="Dashboard" descrisão={"Acompanhe de maneira geral sua empresa"}/>
+      <Title title="Dashboard" descricao={"Acompanhe de maneira geral sua empresa"}/>
 
         <QuickInformations cards={[
-            {title: "Justificativa do Mês", value: 47, icon: icons.justification },
-            {title: "Ativos",  value: 47, color: "green"},
-            {title: "Inativos",  value: 47 },
-            {title: "Departamentos", value: 47 }
+            {title: "Justificativa do Mês", value: 47, icon: icons.justification},
+            {title: "Presença Média",  value: 99.9, color: "green", subtitle: "%"},
+            {title: "Tempo médio de resposta",  value: 47 },
+            {title: "Performance excelente", value: 47 }
         ]}
         />
-      <div className='flex p-4'>
-
-
-        <div className=' p-4 w-3/5'>
-          <JustificativasPendentes
-            nome={"Raphaela"}
-            cargo={"Auxiliar de TI"}
-            motivo={"Consulta Médica"}
-            data={"23/12"}
-            prioridade={"Alta prioridade"}
-            horaRestante={"3"}
-          />
-        </div>
-        <div className='p-4 w-2/5'>
-          <Weekly
-            numberRequestsAproved={3}
-            numberRequestsRejected={2}
-            aprovedPercentage={80}
-          />
-        </div>
-      </div>
     </div>
   )
 }
