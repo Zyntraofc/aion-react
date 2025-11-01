e3d9159dc905368cadf8b449c0004f66468b4ac1
RaphaelaMariaGomes <raphaelamsg@gmail.com>
2025-11-01 18:15:23 -0300

    fix: arrumando o togglesidebar

--- src/components/header/index.jsx
+++ src/components/header/index.jsx
@@ -1,22 +1,25 @@
 import UserProfile from "../userProfile";
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import ToogleSidebar from "../toogleSidebar";
 
 function Header({ onToggle }) {
-    const [nome] = useState('Raphaela')
-    const [cargo] = useState('Auxiliar de TI')
-    const [email] = useState('raphaela@gmail.com')
-    const [foto] = useState('')
+    const [isMobile, setIsMobile] = useState(window.innerWidth < 768);
+
+    useEffect(() => {
+        const handleResize = () => setIsMobile(window.innerWidth < 768);
+        window.addEventListener('resize', handleResize);
+        return () => window.removeEventListener('resize', handleResize);
+    }, []);
 
     return (
         <div className="w-full">
             <div className="flex items-center justify-between p-4 mt-1">
-                <div className="flex items-center space-x-4">
-                    <ToogleSidebar onToggle={onToggle} />
-                </div>
-                <div className="flex items-center">
-                    <UserProfile nome={nome} cargo={cargo} email={email} foto={foto}/>
-                </div>
+                {!isMobile && (
+                    <div className="flex items-center space-x-4">
+                        <ToogleSidebar onToggle={onToggle} />
+                    </div>
+                )}
+
             </div>
         </div>
     );
